d3161aa119b0a73987fc792a6b9f862e475bb1a8
Mehar Ali <mehar0alii@gmail.com>
2023-12-08 10:50:38 +0500

    added react-responsive package for logo

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,13 +1,15 @@
 "use client";
-import Link from 'next/link'
-import React, { useState, useRef, useEffect } from 'react'
+import Link from "next/link";
+import React, { useState, useRef, useEffect } from "react";
 import { IoPersonCircleSharp } from "react-icons/io5";
 import { RiMenu3Line } from "react-icons/ri";
 import { RxCross2 } from "react-icons/rx";
-import { mulish } from '../fonts';
+import { mulish } from "../fonts";
+import { useMediaQuery } from "react-responsive";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const isSmallDevice = useMediaQuery({ query: "(max-width: 768px)" });
   const menuRef = useRef(null);
 
   useEffect(() => {
@@ -17,10 +19,10 @@ const Navbar = () => {
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
@@ -29,31 +31,39 @@ const Navbar = () => {
   };
 
   return (
-    <main className='w-screen flex justify-between items-center px-10 py-4'>
+    <main className="w-screen flex justify-between items-center px-10 py-4">
       {/* Logo */}
-        <Link href={'/'} className={`text-3xl font-bold ${mulish.className}`}>Movies<span className='italic text-yellow-400'>Hub</span></Link>
-        {/* Menus Tabs */}
-        <div className='flex space-x-5 text-xl'>
-          {/* Profile Logo */}
-            <p className='cursor-pointer'><IoPersonCircleSharp /></p>
-            {/* Menubar */}
-            <p className='cursor-pointer relative' onClick={handleOnClick}>
-              {isOpen ? <RxCross2 /> : <RiMenu3Line />}
-              {isOpen && (
-                <ul
-                  ref={menuRef}
-                  className='absolute top-8 right-3 w-fit border-2 py-2 px-3 text-sm font-semibold rounded-lg space-y-3 bg-white text-black'
-                >
-                  <Link href={'/'}>Home</Link>
-                  <Link href={'/'}>About</Link>
-                  <Link href={'/'}>Contact</Link>
-                  <Link href={'/'}>Feedback</Link>
-                </ul>
-              )}
-            </p>
-        </div>
+      <Link href={"/"} className={`text-3xl font-bold ${mulish.className}`}>
+        {isSmallDevice ? (
+          <div className="flex">M<p className="italic text-yellow-400">H</p></div>
+        ) : (
+          <div className="flex">Movies<p className="italic text-yellow-400">Hub</p></div>
+        )}
+      </Link>
+      {/* Menus Tabs */}
+      <div className="flex space-x-5 text-xl">
+        {/* Profile Logo */}
+        <p className="cursor-pointer">
+          <IoPersonCircleSharp />
+        </p>
+        {/* Menubar */}
+        <p className="cursor-pointer relative" onClick={handleOnClick}>
+          {isOpen ? <RxCross2 /> : <RiMenu3Line />}
+          {isOpen && (
+            <ul
+              ref={menuRef}
+              className="absolute top-8 right-3 w-fit border-2 py-2 px-3 text-sm font-semibold rounded-lg space-y-3 bg-white text-black"
+            >
+              <Link href={"/"}>Home</Link>
+              <Link href={"/"}>About</Link>
+              <Link href={"/"}>Contact</Link>
+              <Link href={"/"}>Feedback</Link>
+            </ul>
+          )}
+        </p>
+      </div>
     </main>
-  )
-}
+  );
+};
 
 export default Navbar;
--- src/app/search/page.js
+++ src/app/search/page.js
@@ -183,13 +183,13 @@ const MovieSearch = () => {
             )}
 
             {isOpen && (
-              <div className="mt-10 mr-4 text-white p-4 rounded-md flex space-x-2 justify-center items-center z-50">
+              <div className="mt-5 mr-4 text-white p-4 rounded-md flex flex-col md:flex-row space-x-2 justify-center items-center z-50">
                 <input
                   type="text"
                   placeholder="Search for a movie..."
                   value={query}
                   onChange={handleInputChange}
-                  className="bg-gray-900 text-white font-semibold px-4 py-2 rounded-full focus:outline-none ring-2 focus:ring-4 ring-yellow-500 mb-2"
+                  className="w-3/4 bg-gray-900 text-white font-semibold px-4 py-2 rounded-full focus:outline-none ring-2 focus:ring-4 ring-yellow-500 mb-2"
                 />
                 <button
                   type="submit"
